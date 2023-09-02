27504c4a7ba43ee1e994b511992e4ff6799998c6
Johan Smit <johansm@bbd.co.za>
2023-09-02 12:13:02 +0200

    Dialogs

--- Assets/Scripts/pickupObjectController.cs
+++ Assets/Scripts/pickupObjectController.cs
@@ -1,9 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Tilemaps;
-using static UnityEditor.PlayerSettings;
 
 public class pickupObjectController : MonoBehaviour
 {
